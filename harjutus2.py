68e8e8fba16821ce9a39905c50d9a04996388131
sjakobson-arch <sjakobson@hkhk.edu.ee>
2025-10-09 14:44:51 +0300

    Add files via upload

---
+++ harjutus2.py
@@ -0,0 +1,46 @@
+import turtle
+
+#kilpkonna seaded
+turtle.speed(0)
+ekraan = turtle.Screen()
+ekraan.title("Olümpiarõngad - Simon")
+ekraan.screensize(500,400)
+turtle.pensize(6)
+
+
+
+#olümpiarõngad
+turtle.pencolor("blue")
+turtle.penup()
+turtle.goto(-110,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("black")
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("red")
+turtle.penup()
+turtle.goto(110,0)
+turtle.pendown()
+turtle.circle(50)    
+
+turtle.pencolor("yellow")
+turtle.penup()
+turtle.goto(-55,-45)
+turtle.pendown()
+turtle.circle(50)    
+
+turtle.pencolor("green")
+turtle.penup()
+turtle.goto(55,-45)
+turtle.pendown()
+turtle.circle(50)    
+    
+    
+
+
+turtle.done()
