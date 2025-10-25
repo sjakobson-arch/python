430104b0bc923c1e38c1bb1f06f503e954eb3fe6
sjakobson-arch <sjakobson@hkhk.edu.ee>
2025-10-25 14:04:45 +0300

    Add files via upload

---
+++ harjutus09.py
@@ -0,0 +1,12 @@
+#   harjutus09
+
+
+for i in range(1,21):
+    if i%15 == 0:
+        print(i,"TIKTAK")
+    elif i%3 == 0:
+        print(i, "TIK")
+    elif i%5 == 0:
+        print(i,"TAK")
+    else:
+        print(i)
