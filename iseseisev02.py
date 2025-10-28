dbf678823c3f593424f41af9369e03c1a86d6a4a
sjakobson-arch <sjakobson@hkhk.edu.ee>
2025-10-28 10:11:48 +0200

    Add files via upload

---
+++ iseseisev02.py
@@ -0,0 +1,16 @@
+
+import turtle
+
+for i in range(4):
+        turtle.fd(100)
+        turtle.lt(90)
+        turtle.fd(100)
+        turtle.lt(90)
+        turtle.fd(100)
+        turtle.rt(90)
+
+
+
+
+
+turtle.done()
