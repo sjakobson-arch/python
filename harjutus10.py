7d46be3be81e3194e4b00d3e3e912912233263a6
sjakobson-arch <sjakobson@hkhk.edu.ee>
2025-11-06 14:03:57 +0200

    Add files via upload

---
+++ harjutus10.py
@@ -0,0 +1,26 @@
+# 
+# Harjutus10
+# 
+# Arvude keskmine
+# Koostage Pythoni programm, mis küsib kasutajalt arve ükshaaval.
+# Programm peaks jätkama arvude küsimist ja nende vastuvõtmist seni, kuni kasutaja jätab sisestuse tühjaks
+# (st vajutab sisestusklahvi ilma midagi kirjutamata).
+# Programm peab kasutama while-tsüklit arvude küsimiseks ja nende salvestamiseks.
+# Pärast seda, kui kasutaja lõpetab arvude sisestamise peab programm arvutama ja väljastama kõikide sisestatud arvude keskmise väärtuse.
+# 
+# # counter = 0
+# # sum = 0
+# # loop = 1
+# # 
+# # 
+# # while loop:
+# #     arv = input("Lisa arv: ")
+# #     if arv=="":
+# #         break #töötab ka loop = 0
+# #     counter+=1
+# #     sum+=int(arv)
+# # print(f"Keskmine: {sum/counter}")
+# #     
+# #     
+# #
+
