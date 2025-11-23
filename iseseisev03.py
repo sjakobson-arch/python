9b347b04384018f3229f467f81e0932fb01c9c4d
sjakobson-arch <sjakobson@hkhk.edu.ee>
2025-11-23 15:51:11 +0200

    Add files via upload
    teine versioon

--- iseseisev03.py
+++ iseseisev03.py
@@ -24,51 +24,57 @@ import random
 #suvaline
 # suvaline = random.randint()
 
-kujund = input("Millist kujundit soovid joonistada - viisnurk, ring, ruut või suvaline?: ")
+kujund = input("1)viisnurk \n2)ring \n3)ruut \n4)suvaline \nMillist kujundit soovid joonistada: ")
 arv = int(input("Mitu kujundit soovid joonistada?: "))
-# input("Soovid jätkata?: ")
+# input("Soovid jätkata?: "
 
+kylg =90
 # viisnurk
-def viisnurk():
-    for i in range(arv):
-        turtle.lt(45)
-        turtle.fd(100)
-        turtle.lt(100)
-        turtle.fd(120)
-        turtle.lt(25)
-        turtle.fd(140)
-        turtle.lt(95)
-        turtle.fd(90)
-        turtle.lt(72)
-        turtle.fd(192)
-viisnurk()
-
-           
+def viisnurk(kylg):
+    for i in range(5):
+        turtle.fd(kylg)
+        turtle.rt(144)
+        
 # ring
-def ring():
-    for i in range(arv):
-        turtle.circle(90)
-ring()
+def ring(kylg):
+    turtle.circle(kylg)
 
 # ruut
-def ruut():
-    for i in range(arv):
-        turtle.fd(100)
+def ruut(kylg):
+    for i in range(4):
+        turtle.fd(kylg)
         turtle.lt(90)
-        turtle.fd(100)
-        turtle.lt(90)
-        turtle.fd(100)
-        turtle.lt(90)
-        turtle.fd(100)
-ruut()
 
 #suvaline
-def suvaline():
-    random.randint()
-
-
-
-
+def suvaline(kylg):
+    kujund(random.choice("1", "2", "3"))
+    arv(random.choice)
+    
+    
+# kujundite joonistamine   
+if kujund=="1":
+    for i in range(arv):
+        viisnurk(kylg)
+        turtle.lt(kylg * 1.5)
+        
+elif kujund=="2":
+    for i in range(arv):
+        ring(kylg)
+        turtle.lt(kylg * 1.5)
+        
+elif kujund=="3":
+    for i in range(arv):
+        ruut(kylg)
+        turtle.lt(kylg * 1.5)
+        
+elif kujund=="4":
+    for i in range(arv):
+        suvaline(kylg)
+    
+    
+else:
+    pass
+        
 
 
 
